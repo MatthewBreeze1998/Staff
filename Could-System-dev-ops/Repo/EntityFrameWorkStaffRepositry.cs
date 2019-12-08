f684e9976a7192815f95a0676ceece74a5da521a
MATTHEW BREEZE <T7077260@live.tees.ac.uk>
2019-12-08 19:54:34 +0000

    Frame work and repo

---
+++ Could-System-dev-ops/Repo/EntityFrameWorkStaffRepositry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Could_System_dev_ops.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Could_System_dev_ops.Repo
+{
+    public class EntityFrameWorkStaffRepositry : IRepository<ProductsModel>
+    {
+        private readonly IServiceScope _scope;
+        private readonly StaffDataBaseContext _context;
+
+        public EntityFrameWorkStaffRepositry(IServiceProvider service)
+        {
+            _scope = service.CreateScope();
+            _context = _scope.ServiceProvider.GetRequiredService<StaffDataBaseContext>();
+
+        }
+        public StaffModel CreateObject(StaffModel Object)
+        {
+            _context.Staff.Add(Object);
+            _context.SaveChanges();
+
+            return Object;
+        }
+        public IEnumerable<StaffModel> GetObject()
+        {
+            return _context.Staff;
+        }
+
+        public bool UpdateObject(StaffModel Object, bool Return)
+        {
+            try
+            {
+                _context.Staff.Update(Object);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Could-System-dev-ops/Repo/IReppsitory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Could_System_dev_ops.Models;
+
+namespace Could_System_dev_ops.Repo
+{
+    public interface IRepository<Product>
+    {
+
+
+
+        bool UpdateObject(StaffModel Object, bool Return);
+
+
+        StaffModel CreateObject(StaffModel Object);
+
+        IEnumerable<Models.StaffModel> GetObject();
+
+
+
+
+
+
+
+    }
+}
